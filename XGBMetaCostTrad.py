a6157edc10973a2ba35c5b6e715d862f097d24ef
astezron <abhishekborah0@gmail.com>
2025-05-23 01:09:38 +0530

    Update XGBMetaCostTrad.py

--- XGBMetaCostTrad.py
+++ XGBMetaCostTrad.py
@@ -19,8 +19,8 @@ warnings.filterwarnings("ignore", category=UserWarning, module="xgboost")
 # ================================
 # 1. Load Dataset
 # ================================
-train_df = pd.read_csv(r"C:\CopperPrj\Final_OUT\Trad_ppm\train_trad.csv")
-test_df = pd.read_csv(r"C:\CopperPrj\Final_OUT\Trad_ppm\test_trad.csv")
+train_df = pd.read_csv(r"Trad_TrainDemo.csv")
+test_df = pd.read_csv(r"Trad_TestDemo.csv")
 
 # ================================
 # 2. Predictors & Target
