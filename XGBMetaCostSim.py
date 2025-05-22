c8babce6f6800a35a862574594dcf746e4b594c9
astezron <abhishekborah0@gmail.com>
2025-05-23 01:08:34 +0530

    Update XGBMetaCostSim.py

--- XGBMetaCostSim.py
+++ XGBMetaCostSim.py
@@ -19,8 +19,8 @@ warnings.filterwarnings("ignore", category=UserWarning, module="xgboost")
 # ================================
 # 1. Load Dataset
 # ================================
-train_df = pd.read_csv(r"C:\CopperPrj\Final_OUT\Simulations\Simu_TrainDemo.csv")
-test_df = pd.read_csv(r"C:\CopperPrj\Final_OUT\Simulations\Simu_TestDemo.csv")
+train_df = pd.read_csv("Simu_TrainDemo.csv")
+test_df = pd.read_csv("Simu_TestDemo.csv")
 
 # ================================
 # 2. Predictors & Target
